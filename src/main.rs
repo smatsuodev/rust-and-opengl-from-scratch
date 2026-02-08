d37eaf0f00a019002f99c70fc7e26fd50ef8737c
smatsuodev <git@smatsuo.dev>
2026-02-08 16:26:36 +0900

    use gl to fill background with purple

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,15 @@ fn main() {
     let video_subsystem = sdl.video().unwrap();
     let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+    let gl_context = window.gl_create_context().unwrap();
+    let gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe { gl::ClearColor(0.3, 0.3, 0.5, 1.0) };
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -18,5 +24,11 @@ fn main() {
                 _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
